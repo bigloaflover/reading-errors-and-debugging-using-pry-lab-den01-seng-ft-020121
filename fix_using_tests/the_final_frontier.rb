566d823ebc3bb191fb8c90999f1e32794449e9a1
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-06 20:31:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -5,12 +5,13 @@ def generate_star_date
 end
 
 
-def state_log(date)
-  "Captain's Log, star date #{date}."
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
 end
 
 
 def engage
   puts state_log(date)
+  binding.pry
   date = generate_star_date
 end
